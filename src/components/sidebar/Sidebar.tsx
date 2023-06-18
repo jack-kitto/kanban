066e31039241f1a77f267f66ce3c5a0be7279b50
Jack Kitto <jack.kitto@gmail.com>
2023-06-18 18:01:38 +1000

    styles sidebar items

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,14 +1,23 @@
 import Image from 'next/image'
 import { SidebarItem } from './SidebarItem';
+import { useState } from 'react';
 export const Sidebar = () => {
+  const items = [
+    "Platform Launch",
+    "Marketing Plan",
+    "Roadmap",
+  ]
+  const [activeItem, setActiveItem] = useState("Platform Launch")
   return (
     <div className="flex flex-col h-full w-1/6 border-2 border-linesLight">
       <div className="flex flex-row items-center  p-10 , h-24 w-full">
         <Image src="/logo.png" alt="Logo" width="24" height="25" />
         <div className=" ml-5 mt-2 text-xl font-bold">kanban</div>
       </div>
-      <div className="flex flex-col items-center justify-center h-24 w-full">
-        <SidebarItem />
+      <div className="gap-3 flex flex-col items-start justify-start h-full w-full">
+        {
+          items.map((item) => <SidebarItem key={item} text={item} onClick={() => console.log(item)} active={activeItem == item} />)
+        }
       </div>
     </div>
   );
--- src/components/sidebar/SidebarItem.tsx
+++ src/components/sidebar/SidebarItem.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Image from 'next/image'
 import Board from '../../../public/icon-board.svg'
 import { typography } from '~/styles/typography';
+import { colors } from '~/styles/colors';
 
 interface SidebarItemProps {
   text: string;
   active: boolean;
   onClick: () => void;
 }
-export const SidebarItem = () => {
+export const SidebarItem = ({ text, active, onClick }: SidebarItemProps) => {
   const styles = {
     active: {
 
@@ -18,21 +19,16 @@ export const SidebarItem = () => {
     },
   }
   return (
-    <>
-      <div className='flex flex-col'>
-        <div className='flex-row flex'>
-          <div className='mr-2 justify-center items-center flex flex-col'>
-            <Image src={Board} alt="Logo" width="16" height="16" />
-          </div>
-          <div style={typography.heading.M}>Heading (M)</div>
+    <button style={{ height: '48px', backgroundColor: active ? colors.mainPurple : undefined }} className='flex flex-col rounded-r-full h-full w-full justify-center pl-6'>
+      <div className='flex-row flex'>
+        <div className='mr-2 justify-center items-center flex flex-col'>
+          <Image src={`/project-regular${active ? "-active" : ""}.png`} alt="boardIcon" width="16" height="16" />
         </div>
-        <div className='flex flex-row'>
-          <div className='mr-2 justify-center items-center flex flex-col'>
-            <Image src={Board} alt="Logo" width="16" height="16" />
-          </div>
-          <div> + Create New Board</div>
-        </div>
+        <div style={{
+          ...typography.heading.M,
+          color: active ? colors.white : colors.darkGrey
+        }}>{text}</div>
       </div>
-    </>
+    </button>
   );
 }
