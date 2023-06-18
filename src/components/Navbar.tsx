868e66e4f95ea993edb54df90a6a05fbb2d93afe
Jack Kitto <jack.kitto@gmail.com>
2023-06-18 12:34:36 +1000

    added base navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 export const Navbar = () => {
   return (
     // a div with a height of 97px and a width that fills the screen
-    <div className="flex h-24 w-auto flex-row bg-slate-50 ">
+    <div className="flex h-24 w-auto flex-row bg-white">
       <div className="flex items-center">
         <div className="mr-4">Login</div>
         <div className="mr-4">Register</div>
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,13 @@
 import type { PropsWithChildren } from "react";
 import { Navbar } from "./Navbar";
 import { Sidebar } from "./sidebar";
+import { colors } from "~/styles/colors";
 
 export const PageLayout = (props: PropsWithChildren) => {
   return (
     <main className="overflow-none flex h-screen justify-center">
       <Sidebar />
-      <div className="flex h-full w-full flex-col bg-blue-50">
+      <div style={{ backgroundColor: colors.lightGrey }} className="flex h-full w-full flex-col">
         <Navbar />
         {props.children}
       </div>
