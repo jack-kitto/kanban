3d1fb921a757727d65f81c8d7df38bef5f8fdd42
Jack Kitto <jack.kitto@gmail.com>
2023-06-18 12:34:26 +1000

    create display for each component for styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,10 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 import { ClerkProvider, useUser, SignIn, SignedOut, SignUp } from '@clerk/nextjs'
 import { PageLayout } from "~/components/layout";
+import { Button } from "~/components/button";
+import { TextField } from "~/components/textField";
+import { Dropdown } from "~/components/dropdown";
+import { SubtaskCheckbox } from "~/components/subtaskCheckbox";
 
 export default function Home() {
 
@@ -11,7 +15,31 @@ export default function Home() {
   return (
     <>
       <PageLayout>
-        <div> hello </div>
+        <div className="flex justify-evenly h-full">
+          <div className="h-full items-center justify-evenly flex flex-col">
+            <Button onPress={() => console.log("hello")} type="primary" theme="light" size="sm" text="Light Button Primary S" />
+            <Button onPress={() => console.log("Hello")} type="primary" theme="light" size="lg" text="Light Button Primary L" />
+            <Button onPress={() => console.log("Hello")} type="primary" theme="dark" size="sm" text="dark Button Primary S" />
+            <Button onPress={() => console.log("Hello")} type="primary" theme="dark" size="lg" text="dark Button Primary L" />
+            <Button onPress={() => console.log("Hello")} type="secondary" theme="light" size="sm" text="Light Button secondary S" />
+            <Button onPress={() => console.log("Hello")} type="secondary" theme="light" size="lg" text="Light Button secondary L" />
+            <Button onPress={() => console.log("Hello")} type="secondary" theme="dark" size="sm" text="dark Button secondary S" />
+            <Button onPress={() => console.log("Hello")} type="secondary" theme="dark" size="lg" text="dark Button secondary L" />
+            <Button onPress={() => console.log("Hello")} type="destructive" theme="light" size="sm" text="Light Button destructive S" />
+            <Button onPress={() => console.log("Hello")} type="destructive" theme="light" size="lg" text="Light Button destructive L" />
+            <Button onPress={() => console.log("Hello")} type="destructive" theme="dark" size="sm" text="dark Button destructive S" />
+            <Button onPress={() => console.log("Hello")} type="destructive" theme="dark" size="lg" text="dark Button destructive L" />
+          </div>
+          <div className="h-full items-center justify-evenly flex flex-col">
+            <TextField />
+          </div>
+          <div className="h-full items-center justify-evenly flex flex-col">
+            <Dropdown />
+          </div>
+          <div className="h-full items-center justify-evenly flex flex-col">
+            <SubtaskCheckbox />
+          </div>
+        </div>
       </PageLayout>
     </>
   );
