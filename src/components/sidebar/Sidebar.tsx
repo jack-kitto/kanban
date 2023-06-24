a603c599de7c5d808b27dee650834346a6815c28
Jack Kitto <jack.kitto@gmail.com>
2023-06-24 21:26:12 +1000

    show projects in side bar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -3,6 +3,8 @@ import { SidebarItem } from './SidebarItem';
 import Modal from 'react-modal';
 import { useState } from 'react';
 import { CreateBoard } from '../createBoard';
+import { api } from '~/utils/api';
+import { Loading } from '../loading';
 export const Sidebar = () => {
   const items = [
     "Platform Launch",
@@ -11,20 +13,23 @@ export const Sidebar = () => {
   ]
   const [activeItem, setActiveItem] = useState("Platform Launch")
   const [modalIsOpen, setIsOpen] = useState(false);
+  const { data, isLoading } = api.projects.getAll.useQuery()
 
   const afterOpenModal = () => { }
   const closeModal = () => { }
 
   Modal.setAppElement('#root');
+  if (isLoading) return <Loading />
   return (
-    <div className="flex flex-col h-full w-1/6 border-2 border-linesLight">
+    <div className={"flex flex-col h-full w-1/6 border-2 border-linesLight"}>
       <div className="flex flex-row items-center  p-10 , h-50 w-full">
         <Image src="/logo.png" alt="Logo" width="24" height="25" />
         <div className=" ml-5 mt-2 text-xl font-bold items-center justify-center h-full w-full">kanban</div>
       </div>
       <div className="gap-3 flex flex-col items-start justify-start h-full w-full">
         {
-          items.map((item) => <SidebarItem key={item} text={item} setActive={setActiveItem} onClick={() => console.log(item)} active={activeItem == item} />)
+          data ? data.map((project) => <SidebarItem key={project.id} text={project.name} setActive={setActiveItem} onClick={() => console.log(project)} active={activeItem == project.name} />)
+            : null
         }
         <button onClick={() => setIsOpen(true)} className='flex flex-col rounded-r-full justify-center pl-6'>
           <div className='flex flex-row'>
