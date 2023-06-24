b3151a446d606ab520967a1776da5fd7a7724064
Jack Kitto <jack.kitto@gmail.com>
2023-06-24 21:15:30 +1000

    add loading component

---
+++ src/components/loading/Loading.tsx
@@ -0,0 +1,10 @@
+import { ClimbingBoxLoader } from "react-spinners";
+
+export const Loading = () => {
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <ClimbingBoxLoader />
+    </div>
+  )
+}
+
