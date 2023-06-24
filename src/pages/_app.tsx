cbf22b975f7bc54e72e6f460164810fb173b3109
Jack Kitto <jack.kitto@gmail.com>
2023-06-24 21:15:19 +1000

    add toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
+import { Toaster } from 'react-hot-toast';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <div id="root">
       <ClerkProvider>
         <Component {...pageProps} />
+        <Toaster />
       </ClerkProvider>
     </div>
   )
