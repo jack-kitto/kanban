5f54dbf8cbd142527bb680daa46cbb1efcd27b5b
Jack Kitto <jack.kitto@gmail.com>
2023-06-24 19:41:45 +1000

    update create project private procedure to create columns with project

--- src/server/api/routers/projects.ts
+++ src/server/api/routers/projects.ts
@@ -5,12 +5,20 @@ export const projectsRouter = createTRPCRouter({
   create: privateProcedure
     .input(z.object({
       name: z.string().min(1).max(32),
+      columns: z.optional(z.array(z.string().min(1).max(32))),
     }))
     .mutation(async ({ ctx, input }) => {
       const response = await ctx.prisma.project.create({
         data: {
           name: input.name,
           userId: ctx.userId,
+          columns: {
+            create: input.columns?.map((column) => {
+              return {
+                name: column,
+              }
+            }),
+          }
         },
       })
       return response
