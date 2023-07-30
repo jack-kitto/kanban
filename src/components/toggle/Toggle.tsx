eb481e4689cae7b8b7b23ad187e012d3bc9c3a97
Jack Kitto <jack.kitto@gmail.com>
2023-07-30 12:45:30 +1000

    add switch to toggle component

--- src/components/toggle/Toggle.tsx
+++ src/components/toggle/Toggle.tsx
@@ -1,7 +1,15 @@
-export const Toggle = () => {
+import React from 'react'
+import { Switch } from '@chakra-ui/react'
+export const Toggle = ({ size, isDisabled, isChecked }:
+  {
+    size: 'sm' | 'md' | 'lg',
+    isDisabled?: boolean,
+    isChecked?: boolean
+  }) => {
   return (
     <div>
       toggle
+      <Switch size={size} isChecked={isChecked} isDisabled={isDisabled} />
     </div>
   )
 }
