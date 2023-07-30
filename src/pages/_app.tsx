3eb13f8a9ade67000efe1697ad725ed9b05e275b
Jack Kitto <jack.kitto@gmail.com>
2023-07-30 12:45:21 +1000

    add chakra provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,17 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
 import { Toaster } from 'react-hot-toast';
+import { ChakraProvider } from '@chakra-ui/react'
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <div id="root">
-      <ClerkProvider>
-        <Component {...pageProps} />
-        <Toaster />
-      </ClerkProvider>
+      <ChakraProvider>
+        <ClerkProvider>
+          <Component {...pageProps} />
+          <Toaster />
+        </ClerkProvider>
+      </ChakraProvider>
     </div>
   )
 };
