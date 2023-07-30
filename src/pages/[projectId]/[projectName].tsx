e41f7332e67e314e401c5a032932265398acde2e
Jack Kitto <jack.kitto@gmail.com>
2023-07-30 11:45:06 +1000

    nest page in pagelayout

--- src/pages/[projectId]/[projectName].tsx
+++ src/pages/[projectId]/[projectName].tsx
@@ -1,13 +1,13 @@
 import { useRouter } from 'next/router'
 import { toast } from 'react-hot-toast'
 import { z } from 'zod'
+import { PageLayout } from '~/components/layout'
 import { Loading } from '~/components/loading'
 import { api } from '~/utils/api'
 export default function Project() {
   const router = useRouter()
   const { projectId, projectName } = router.query
   const id = z.number().safeParse(parseInt(`${projectId}`))
-  const name = z.string().safeParse(`${projectName}`)
   if (!id.success) {
     toast('Invalid ID')
     return null
@@ -21,9 +21,11 @@ export default function Project() {
   if (isLoading) return <Loading />
   console.log(data)
   return (
-    <div>
-      <div>projectId {projectId}</div>
-      <div>projectName {projectName}</div>
-    </div>
+    <PageLayout>
+      <div>
+        <div>projectId {id.data}</div>
+        <div>projectName {projectName}</div>
+      </div>
+    </PageLayout>
   )
 }
