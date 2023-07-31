1a4c8ba58c2f47cae0cae59e08a59526e5760872
Jack Kitto <jack.kitto@gmail.com>
2023-07-31 19:55:22 +1000

    create icon component

---
+++ src/components/icon/Icon.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image"
+
+interface IconProps {
+  icon: keyof typeof iconRegistry,
+  size?: keyof typeof sizes
+  alt?: string
+}
+export const Icon = ({ icon, size = 'normal', alt = "icon" }: IconProps) => {
+
+  return (
+    <Image src={iconRegistry[icon]} alt={alt} width={`${sizes[size]}`} height={`${sizes[size]}`} />
+  )
+
+}
+
+const sizes = {
+  normal: 16,
+  small: 12,
+  medium: 20,
+  large: 24,
+}
+
+const iconRegistry = {
+  projectRegular: '/project-regular.png',
+  logo: '/logo.png',
+  sun: '/sun.png',
+  darkTheme: '/darkTheme.png',
+}
