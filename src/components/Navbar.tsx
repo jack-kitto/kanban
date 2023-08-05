108c239fd8d4eab95e5fa68833f3efbba56dc98b
Jack Kitto <jack.kitto@gmail.com>
2023-08-05 16:54:46 +1000

    move profile button to left of modal and add options and add new task button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,31 +2,73 @@ import { ClerkProvider, useUser, SignIn, SignedOut, SignUp, SignOutButton, SignI
 import { observer } from 'mobx-react-lite'
 import { useStores } from '~/models'
 import { Icon } from './icon'
+import { Button } from './button'
+import { toast } from 'react-hot-toast'
+import { MainModal } from './mainModal'
+import { typography } from '~/styles/typography'
+import { useState } from 'react'
+import { api } from '~/utils/api'
 const NavbarComponent = () => {
   const { isLoaded, isSignedIn, user } = useUser()
   const { theme } = useStores()
+  const [open, setOpen] = useState(false)
+  const ctx = api.useContext()
+  const { mutate, isLoading } = api.projects.deleteProjectById.useMutation({
+    onSuccess: () => {
+    },
+    onError: (e: any) => {
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+      toast(errorMessage)
+    },
+  });
+
   if (!isLoaded) return null
   if (!user) return null
-  console.log(user)
   return (
-    // a div with a height of 97px and a width that fills the screen
     <div className={`flex h-24 w-auto flex-row bg-${theme.darkMode ? "darkGrey" : "white"} border-b-2 border-${theme.darkMode ? "linesDark" : "linesLight"}`}>
-      <div className="flex items-center w-full ml-6 mr-6">
-        {
-          isSignedIn ?
-            <div className='w-full flex justify-end items-center'>
-              <UserButton />
-              <div className='m-4 hover:opacity-50 cursor-pointer'>
-                <Icon icon="options" size='xxxs' />
+      <div className="flex justify-between items-center w-full ml-6 mr-6">
+        <div>
+          {
+            isSignedIn ?
+              <div className='w-full flex justify-end items-center'>
+                <UserButton />
               </div>
-            </div>
-            :
-            <div>
-              <SignInButton />
-              <SignUpButton />
-            </div>
-
-        }
+              :
+              <div>
+                <SignInButton />
+                <SignUpButton />
+              </div>
+          }
+        </div>
+        <div className='flex flex-row'>
+          <div>
+            <Button fitText type='primary' size='lg' text='+ Add New Task' onPress={() => toast("Adding new task")} />
+          </div>
+          <div className='m-4 hover:opacity-50 cursor-pointer'>
+            <button onClick={() => setOpen(true)} className='cursor-pointer'>
+              <Icon icon="options" size='xxxs' />
+            </button>
+            <MainModal size='2xl' open={open} setOpen={setOpen} onClose={() => setOpen(false)}
+              header={
+                <div className='mt-6'>
+                  <p style={typography.heading.L} className='text-red'>Delete this board?</p>
+                </div>
+              }
+              body={
+                <div>
+                  <p>Are you sure you want to delete the ‘Platform Launch’ board? This action will remove all columns and tasks and cannot be reversed.</p>
+                  <div className='flex flex-row flex-1 mt-8 mb-12'>
+                    <div className='flex flex-col flex-1'>
+                      <Button type='destructive' size='lg' text='Delete Board' onPress={() => mutate({ id: "as" })} />
+                    </div>
+                    <div className='flex flex-col flex-1'>
+                      <Button type='secondary' size='lg' text='Cancel' onPress={() => setOpen(false)} />
+                    </div>
+                  </div>
+                </div>
+              } />
+          </div>
+        </div>
       </div>
     </div>
   );
