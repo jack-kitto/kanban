1d67457156083ca71bfad46c73f4988256cb2e20
Jack Kitto <jack.kitto@gmail.com>
2023-08-05 16:54:13 +1000

    create main modal component

---
+++ src/components/mainModal/MainModal.tsx
@@ -0,0 +1,55 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react'
+import { modalAnatomy as parts } from '@chakra-ui/anatomy'
+import {
+  createMultiStyleConfigHelpers,
+  defineStyle,
+} from '@chakra-ui/styled-system'
+const { definePartsStyle, defineMultiStyleConfig } =
+  createMultiStyleConfigHelpers(parts.keys)
+
+interface modalProps {
+  open: boolean
+  setOpen: (open: boolean) => void
+  onClose: () => void
+  header?: React.ReactNode
+  body?: React.ReactNode
+  footer?: React.ReactNode
+  size: "xxs" | "xs" | "sm" | "md" | "lg" | "xl" | "2xl" | "3xl" | "4xl" | "5xl" | "6xl" | "full"
+}
+export const MainModal = ({ open, setOpen, onClose, header, body, footer, size }: modalProps) => {
+
+  return (
+    <div>
+      <Modal isCentered size={size} isOpen={open} onClose={() => onClose && onClose()}>
+        <ModalOverlay />
+        <ModalContent>
+          {header && (
+            <ModalHeader>
+              {header}
+            </ModalHeader>
+          )}
+          <ModalCloseButton />
+          {body && (
+            <ModalBody>
+              {body}
+            </ModalBody>
+          )}
+          {footer && (
+            <ModalFooter>
+              {footer}
+            </ModalFooter>
+          )}
+        </ModalContent>
+      </Modal>
+    </div>
+  )
+
+}
