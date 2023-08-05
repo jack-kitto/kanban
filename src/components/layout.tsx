1236b4156b7acad0c1a0eeb4ee189fe038b61fe5
Jack Kitto <jack.kitto@gmail.com>
2023-08-05 13:20:29 +1000

    add darkmode to page layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,15 +2,19 @@ import type { PropsWithChildren } from "react";
 import { Navbar } from "./Navbar";
 import { Sidebar } from "./sidebar";
 import { colors } from "~/styles/colors";
+import { observer } from "mobx-react-lite";
+import { useStores } from "~/models";
 
-export const PageLayout = (props: PropsWithChildren) => {
+const PageLayoutComponent = (props: PropsWithChildren) => {
+  const { theme } = useStores()
   return (
     <main className="overflow-none flex h-screen justify-center">
       <Sidebar />
-      <div style={{ backgroundColor: colors.lightGrey }} className="flex h-full w-full flex-col">
+      <div style={{ backgroundColor: theme.darkMode ? colors.veryDarkGrey : colors.lightGrey }} className="flex h-full w-full flex-col">
         <Navbar />
         {props.children}
       </div>
     </main>
   );
 };
+export const PageLayout = observer(PageLayoutComponent);
