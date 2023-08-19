fe894a989ead72748b8e386d317de6ff64d7222c
Jack Kitto <jack.kitto@gmail.com>
2023-08-19 12:56:39 +1000

    add styles for board cols and tasks

---
+++ src/components/board/styles.ts
@@ -0,0 +1,36 @@
+export const useStyles = () => {
+  return {
+    container: {
+      height: '100%',
+    },
+    columns: {
+      display: 'grid',
+      height: '100%',
+      gridAutoFlow: 'column',
+      gridAutoColumns: '300px',
+    },
+    col(col: number) {
+      return {
+        display: 'grid',
+        height: '100%',
+        gridAutoFlow: 'row',
+        gridAutoRows: '100px',
+        gridTemplateRows: '50px 100px',
+        gridColumnStart: col,
+      }
+    },
+    addColumn: {
+      gridAutoFlow: 'row',
+      gridAutoRows: '100px',
+      gridTemplateRows: '50px 100px',
+      display: 'grid',
+      height: '100%',
+    },
+    title: {
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    task: {
+    },
+  }
+}
