1d1cb77bb0d691ece3eae927f379ea0a1c1ef11e
jack-kitto1 <jack.kitto@thenod.app>
2024-05-18 12:32:33 +1000

    feat: Add label to select inputs

--- src/components/atoms/select/Select.tsx
+++ src/components/atoms/select/Select.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback } from "react";
 
 export interface SelectProps {
+  label?: string;
   options: string[];
   selected: string;
   setSelected: (selected: string) => void;
@@ -13,14 +14,23 @@ export default function Select(props: SelectProps): JSX.Element {
   }, [props.setSelected]);
 
   return (
-    <select
-      className="bg-white outline caret-mainPurple outline-linesLight border-transparent border-r-[14px] bg-blend-color-dodge prose-bl hover:outline-mainPurple accent-mainPurple px-4 min-h-10 max-h-10 min-w-64 max-w-md rounded h-10 w-full prose-hm transition-colors duration-300 ease-in-out"
-      value={props.selected}
-      onChange={handleChange}
-    >
-      {props.options.map((option, i) => (
-        <option className="prose-bl" key={i} value={option}>{option}</option>
-      ))}
-    </select>
+    <div>
+      {
+        props.label && (
+          <span className="prose-bm text-mediumGray">
+            {props.label}
+          </span>
+        )
+      }
+      <select
+        className="bg-white outline caret-mainPurple outline-linesLight border-transparent border-r-[14px] bg-blend-color-dodge prose-bl hover:outline-mainPurple accent-mainPurple px-4 min-h-10 max-h-10 min-w-64 max-w-md rounded h-10 w-full prose-hm transition-colors duration-300 ease-in-out"
+        value={props.selected}
+        onChange={handleChange}
+      >
+        {props.options.map((option, i) => (
+          <option className="prose-bl" key={i} value={option}>{option}</option>
+        ))}
+      </select>
+    </div>
   )
 }
