564afc6268e3d084fc5cb4ec346914838ae00bb8
jack-kitto1 <jack.kitto@thenod.app>
2024-05-18 13:18:12 +1000

    feat: Add dark theme to tasks

--- src/components/molecules/task/Task.stories.tsx
+++ src/components/molecules/task/Task.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import Task from './Task';
+import { colors } from '~/styles';
 
 const meta = {
   component: Task,
@@ -18,5 +19,25 @@ export const Default: Story = {
   args: {
     title: "Title",
     subtitle: "Subtitle"
+  }
+};
+
+export const Dark: Story = {
+  args: {
+    title: "Title",
+    subtitle: "Subtitle"
+  },
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [{ name: 'dark', value: colors.veryDarkGray }]
+    }
+  },
+  render: function Render(args) {
+    return (
+      <div className="dark">
+        <Task {...args} />
+      </div>
+    )
   }
 };
