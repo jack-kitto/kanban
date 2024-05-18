733828adaef923d46e2221aff050a197347e95e4
jack-kitto1 <jack.kitto@thenod.app>
2024-05-18 16:44:15 +1000

    fix: fix subtask width so it fills container

--- src/components/molecules/subtask/Subtask.tsx
+++ src/components/molecules/subtask/Subtask.tsx
@@ -10,7 +10,7 @@ export default function Subtask(props: SubtaskProps): JSX.Element {
   const { checked, setChecked, text } = props;
   const checkStyle = checked ? 'line-through' : 'none';
   return (
-    <button onClick={() => setChecked(!checked)} className="flex gap-4 items-start cursor-pointer transition-colors duration-300 ease-in-out justify-start bg-linesLight dark:bg-veryDarkGray p-3 hover:bg-mainPurpleHover dark:hover:bg-mainPurple tran rounded max-w-[416px] min-w-[295px]">
+    <button onClick={() => setChecked(!checked)} className="flex gap-4 items-start cursor-pointer transition-colors duration-300 ease-in-out justify-start bg-linesLight dark:bg-veryDarkGray p-3 hover:bg-mainPurpleHover dark:hover:bg-mainPurple rounded w-full min-w-[295px]">
       <div className="flex justify-center">
         <Checkbox checked={checked} setChecked={setChecked} />
       </div>
