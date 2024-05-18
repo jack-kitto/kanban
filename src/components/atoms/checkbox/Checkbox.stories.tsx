5180e53d366d907047dcdd706ac0b5e1bfb08c4c
jack-kitto1 <jack.kitto@thenod.app>
2024-05-18 13:05:51 +1000

    feat: add dark theme and pointer cursor to checkbox

--- src/components/atoms/checkbox/Checkbox.stories.tsx
+++ src/components/atoms/checkbox/Checkbox.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { useArgs } from '@storybook/preview-api';
 import Checkbox, { CheckboxProps } from "./Checkbox";
+import { colors } from "~/styles";
 
 const meta: Meta = {
   tags: ["autodocs"],
@@ -29,6 +30,33 @@ export const Default: Story = {
           updateArgs({ checked });
         }}
       />
+    )
+  }
+}
+
+export const Dark: Story = {
+  args: {
+    checked: false,
+    setChecked: (_: boolean): void => { },
+  },
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [{ name: 'dark', value: colors.darkGray }]
+    }
+  },
+  render: function Render(args: CheckboxProps): JSX.Element {
+    const [{ checked }, updateArgs] = useArgs();
+    return (
+      <div className="dark">
+        <Checkbox
+          {...args}
+          checked={checked}
+          setChecked={(checked: boolean) => {
+            updateArgs({ checked });
+          }}
+        />
+      </div>
     )
   }
 }
--- src/components/atoms/checkbox/Checkbox.tsx
+++ src/components/atoms/checkbox/Checkbox.tsx
@@ -8,7 +8,7 @@ export interface CheckboxProps {
 export default function Checkbox(props: CheckboxProps): JSX.Element {
   return (
     <input
-      className="bg-white accent-mainPurple appearance-none checked:appearance-auto hover:accent-mainPurple hover:brightness-100 brightness-100  checked:border-none border-2 border-linesLight hover:border-linesLight rounded-sm h-4 w-4 highlight-none active:scale-90 transition-transform hover:scale-125 hover:duration-150 ease-in-out select-none checked:bg-mainPurple "
+      className="bg-white cursor-pointer dark:bg-darkGray accent-mainPurple appearance-none checked:appearance-auto hover:accent-mainPurple hover:brightness-100 brightness-100  checked:border-none border-2 dark:hover:border-[#828FA3] dark:border-[#828FA3] border-linesLight hover:border-linesLight rounded-sm h-4 w-4 highlight-none active:scale-90 transition-transform hover:scale-125 hover:duration-150 ease-in-out select-none checked:bg-mainPurple "
       type="checkbox"
       checked={props.checked}
       onChange={(e: ChangeEvent<HTMLInputElement>): void => props.setChecked(e.target.checked)}
