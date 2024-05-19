189c58c6e7bfb4cc855a5dc555d132ba7699ea27
jack-kitto1 <jack.kitto@thenod.app>
2024-05-19 14:56:09 +1000

    fix: Create add new task button

---
+++ src/components/organisms/addTaskButton/AddTaskButton.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { Button, Modal } from "~/components/atoms";
+import type { Task } from "~/components/types";
+import TaskDetail from "../taskDetail/TaskDetail";
+
+export interface AddTaskButtonProps {
+  task: Task;
+  columns: string[];
+  updateTask: (task: Task) => void;
+  onDeleteTask: (task: Task) => void;
+}
+
+export default function AddTaskButton(props: AddTaskButtonProps): JSX.Element {
+  const [showTaskDetail, setShowTaskDetail] = useState<boolean>(false)
+  const [editing, setEditing] = useState<boolean>(true)
+  const [newTask, setNewTask] = useState<boolean>(true)
+
+  return (
+    <>
+      <Button
+        btn={{ onClick: (): void => setShowTaskDetail(true) }}
+        text="+ Add New Task"
+        type="primary"
+      />
+      <Modal
+        open={showTaskDetail}
+        close={(): void => {
+          setShowTaskDetail(false)
+          setEditing(true)
+          setNewTask(true)
+        }}
+      >
+        <TaskDetail
+          columns={props.columns}
+          visible={showTaskDetail}
+          newTask={newTask}
+          editing={editing}
+          setEditing={setEditing}
+          menuOptions={[
+            {
+              onClick: (): void => { setEditing(true) },
+              text: 'Edit Task'
+            },
+            {
+              destructive: true,
+              onClick: (): void => { (): void => props.onDeleteTask(props.task) },
+              text: 'Delete Task'
+            }
+          ]}
+          saveChanges={(task: Task): void => {
+            props.updateTask(task)
+            setNewTask(false)
+          }}
+        />
+      </Modal>
+
+    </>
+  );
+}
--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -1,8 +1,10 @@
 import BoardDetail from "./boardDetail/BoardDetail";
 import TaskDetail from "./taskDetail/TaskDetail";
 import Task from "./task/Task";
+import AddTaskButton from "./addTaskButton/AddTaskButton";
 
 export {
+  AddTaskButton,
   Task,
   BoardDetail,
   TaskDetail,
