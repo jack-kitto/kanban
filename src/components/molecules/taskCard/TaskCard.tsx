ca264a6b910b63d599894e9b9d72a79669d7fef4
jack-kitto1 <jack.kitto@thenod.app>
2024-05-19 18:56:29 +1000

    fix: fix alignment of task card content

--- src/components/molecules/taskCard/TaskCard.tsx
+++ src/components/molecules/taskCard/TaskCard.tsx
@@ -21,18 +21,20 @@ export default function TaskCard(props: TaskCardProps): JSX.Element {
 
   return (
     <Draggable key={`${props.id}`} draggableId={`${props.id}`} index={props.index}>
-      {(dragProvided, dragSnapshot) => (
-        <div
-          ref={dragProvided.innerRef}
-          {...dragProvided.draggableProps}
-          {...dragProvided.dragHandleProps}
-          className="flex bg-white flex-col mb-5 dark:bg-darkGray gap-3 min-h-[88px] min-w-[280px] rounded-lg px-4 py-7"
-          style={{ boxShadow: '0px 4px 6px rgba(54, 78, 126, 0.101545)', ...dragProvided.draggableProps.style }}
-        >
-          <h1 className="prose-hm dark:text-white">{props.title}</h1>
-          <p className="prose-bm text-mediumGray">{props.subtitle}</p>
-        </div>
-      )}
+      {(dragProvided, dragSnapshot) => {
+        return (
+          <div
+            ref={dragProvided.innerRef}
+            {...dragProvided.draggableProps}
+            {...dragProvided.dragHandleProps}
+            className="flex bg-white flex-col items-start mb-5 dark:bg-darkGray gap-3 min-h-[88px] min-w-[280px] rounded-lg px-4 py-7"
+            style={{ boxShadow: '0px 4px 6px rgba(54, 78, 126, 0.101545)', ...dragProvided.draggableProps.style }}
+          >
+            <h1 className="prose-hm dark:text-white">{props.title}</h1>
+            <p className="prose-bm text-mediumGray">{props.subtitle}</p>
+          </div>
+        )
+      }}
     </Draggable>
   )
 }
