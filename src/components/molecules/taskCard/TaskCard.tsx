da3f7124bd2b701ab98210bb7aa1ec6b2efaecb2
jack-kitto1 <jack.kitto@thenod.app>
2024-05-19 14:11:19 +1000

    fix: Add white background to card

--- src/components/molecules/taskCard/TaskCard.tsx
+++ src/components/molecules/taskCard/TaskCard.tsx
@@ -6,7 +6,7 @@ export interface TaskCardProps {
 export default function TaskCard(props: TaskCardProps): JSX.Element {
   return (
     <div
-      className="flex flex-col dark:bg-darkGray gap-3 min-h-[88px] min-w-[280px] rounded-lg px-4 py-7"
+      className="flex bg-white flex-col dark:bg-darkGray gap-3 min-h-[88px] min-w-[280px] rounded-lg px-4 py-7"
       style={{ boxShadow: '0px 4px 6px rgba(54, 78, 126, 0.101545)' }}
     >
       <h1 className="prose-hm dark:text-white">{props.title}</h1>
