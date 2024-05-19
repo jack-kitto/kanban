7284bc0d509b59891fc2d589d3542b4d558e551c
jack-kitto1 <jack.kitto@thenod.app>
2024-05-19 11:55:35 +1000

    refactor: move editing logic into subtask

--- src/components/molecules/subtask/Subtask.stories.tsx
+++ src/components/molecules/subtask/Subtask.stories.tsx
@@ -19,15 +19,45 @@ type Story = StoryObj<typeof meta>;
 export const Light: Story = {
   args: {
     checked: true,
+    onDelete: (): void => { console.log("onDelete") },
+    editing: false,
+    setText: (): void => { console.log("setText") },
     setChecked: (): void => { console.log("setChecked") },
     text: "text"
   },
   render: function Render(args: SubtaskProps): JSX.Element {
-    const [{ checked }, updateArgs] = useArgs<SubtaskProps>();
+    const [{ checked, text }, updateArgs] = useArgs<SubtaskProps>();
     return (
       <Subtask
         {...args}
         checked={checked}
+        text={text}
+        setText={(text: string) => { updateArgs({ text }) }}
+        setChecked={(checked: boolean) => {
+          updateArgs({ checked });
+        }}
+      />
+    )
+  }
+};
+
+export const LightEditing: Story = {
+  args: {
+    onDelete: (): void => { console.log("onDelete") },
+    checked: true,
+    setText: (): void => { console.log("setText") },
+    editing: true,
+    setChecked: (): void => { console.log("setChecked") },
+    text: "text"
+  },
+  render: function Render(args: SubtaskProps): JSX.Element {
+    const [{ checked, text }, updateArgs] = useArgs<SubtaskProps>();
+    return (
+      <Subtask
+        {...args}
+        checked={checked}
+        text={text}
+        setText={(text: string) => { updateArgs({ text }) }}
         setChecked={(checked: boolean) => {
           updateArgs({ checked });
         }}
@@ -38,7 +68,10 @@ export const Light: Story = {
 
 export const Dark: Story = {
   args: {
+    onDelete: (): void => { console.log("onDelete") },
+    setText: (): void => { console.log("setText") },
     checked: true,
+    editing: false,
     setChecked: (): void => { console.log("setChecked") },
     text: "text"
   },
@@ -49,11 +82,45 @@ export const Dark: Story = {
     }
   },
   render: function Render(args: SubtaskProps): JSX.Element {
-    const [{ checked }, updateArgs] = useArgs<SubtaskProps>();
+    const [{ checked, text }, updateArgs] = useArgs<SubtaskProps>();
     return (
       <div className='dark'>
         <Subtask
           {...args}
+          checked={checked}
+          text={text}
+          setText={(text: string) => { updateArgs({ text }) }}
+          setChecked={(checked: boolean) => {
+            updateArgs({ checked });
+          }}
+        />
+      </div>
+    )
+  }
+};
+export const DarkEditing: Story = {
+  args: {
+    checked: true,
+    editing: false,
+    onDelete: (): void => { console.log("onDelete") },
+    setText: (): void => { console.log("setText") },
+    setChecked: (): void => { console.log("setChecked") },
+    text: "text"
+  },
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [{ name: 'dark', value: colors.darkGray }]
+    }
+  },
+  render: function Render(args: SubtaskProps): JSX.Element {
+    const [{ checked, text }, updateArgs] = useArgs<SubtaskProps>();
+    return (
+      <div className='dark'>
+        <Subtask
+          {...args}
+          text={text}
+          setText={(text: string) => { updateArgs({ text }) }}
           checked={checked}
           setChecked={(checked: boolean) => {
             updateArgs({ checked });
--- src/components/molecules/subtask/Subtask.tsx
+++ src/components/molecules/subtask/Subtask.tsx
@@ -1,16 +1,45 @@
-import { Checkbox } from "~/components/atoms";
+import { z } from "zod";
+import { Checkbox, TextField } from "~/components/atoms";
+import { Icon } from "~/components/atoms/icon";
+import { colors } from "~/styles";
 
 export interface SubtaskProps {
   checked: boolean;
   setChecked: (checked: boolean) => void;
   text: string;
+  editing: boolean;
+  setText: (text: string) => void;
+  onDelete: () => void;
 }
 
 export default function Subtask(props: SubtaskProps): JSX.Element {
-  const { checked, setChecked, text } = props;
+  const { checked, setChecked, text, setText } = props;
   const checkStyle = checked ? 'line-through' : 'none';
+
+  if (props.editing) {
+    return (
+      <div className="flex gap-2 justify-between w-full items-center ">
+        <TextField
+          placeholder="e.g. Take coffee break"
+          text={text}
+          validationErrors={[{ message: 'Required', schema: z.string().min(1), active: false }]}
+          setText={setText}
+        />
+        <Icon
+          onClick={props.onDelete}
+          icon="Close"
+          color={colors.mediumGray}
+          size="small"
+        />
+      </div>
+    )
+  }
+
   return (
-    <button onClick={() => setChecked(!checked)} className="flex gap-4 items-start cursor-pointer transition-colors duration-300 ease-in-out justify-start bg-linesLight dark:bg-veryDarkGray p-3 hover:bg-mainPurpleHover dark:hover:bg-mainPurple rounded w-full min-w-[295px]">
+    <button
+      onClick={(): void => setChecked(!checked)}
+      className="flex gap-4 items-start cursor-pointer transition-colors duration-300 ease-in-out justify-start bg-linesLight dark:bg-veryDarkGray p-3 hover:bg-mainPurpleHover dark:hover:bg-mainPurple rounded w-full min-w-[295px]"
+    >
       <div className="flex justify-center">
         <Checkbox checked={checked} setChecked={setChecked} />
       </div>
