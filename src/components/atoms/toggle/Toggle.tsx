4efb8d7a74098911a26c0377d33653dfa220118f
jack-kitto1 <jack.kitto@thenod.app>
2024-05-22 07:20:13 +1000

    feat: Make darkmode toggle update browser storage

--- src/components/atoms/toggle/Toggle.tsx
+++ src/components/atoms/toggle/Toggle.tsx
@@ -3,12 +3,22 @@ import type { ChangeEvent } from "react";
 export interface ToggleProps {
   checked: boolean;
   setChecked: (checked: boolean) => void;
+  onChecked?: () => void;
+  onUnchecked?: () => void;
 }
 
 export default function Toggle(props: ToggleProps): JSX.Element {
   return (
     <label className="relative inline-flex cursor-pointer items-center">
-      <input id="switch" type="checkbox" className="peer sr-only" onChange={(e: ChangeEvent<HTMLInputElement>): void => props.setChecked(e.target.checked)} checked={props.checked} />
+      <input id="switch" type="checkbox" className="peer sr-only" onChange={(e: ChangeEvent<HTMLInputElement>): void => {
+        props.setChecked(e.target.checked)
+        if (e.target.checked) {
+          props.onChecked && props.onChecked();
+        }
+        if (!e.target.checked) {
+          props.onUnchecked && props.onUnchecked();
+        }
+      }} checked={props.checked} />
       <label htmlFor="switch" className="hidden"></label>
       <div className="peer h-6 w-11 rounded-full  bg-mainPurple after:absolute after:left-[8px] after:top-[5px] after:h-[14px] after:w-[14px] after:rounded-full  after:bg-white after:transition-all after:content-[''] peer-checked:bg-mainPurple peer-checked:after:translate-x-full peer-checked:after:border-white outline-none"></div>
     </label>
--- src/components/molecules/darkModeToggle/DarkModeToggle.tsx
+++ src/components/molecules/darkModeToggle/DarkModeToggle.tsx
@@ -1,17 +1,30 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Toggle } from "~/components/atoms";
 import { Icon } from "~/components/atoms/icon";
 
 export interface DarkModeToggleProps {
-  darkMode: boolean;
-  setDarkMode: (darkMode: boolean) => void;
+  darkMode?: boolean;
+  setDarkMode?: (darkMode: boolean) => void;
 }
 
 export default function DarkModeToggle(props: DarkModeToggleProps): JSX.Element {
+  const [darkMode, setDarkMode] = useState<boolean>(props.darkMode ?? false);
+
   return (
     <div className="flex  gap-6 rounded-md py-[14px] items-center justify-center bg-linesLight dark:bg-veryDarkGray">
       <Icon icon="LightMode" />
-      <Toggle checked={props.darkMode} setChecked={props.setDarkMode} />
+      <Toggle
+        checked={darkMode}
+        setChecked={setDarkMode}
+        onChecked={() => {
+          window.localStorage.setItem('darkMode', 'true');
+          props.setDarkMode && props.setDarkMode(true);
+        }}
+        onUnchecked={() => {
+          window.localStorage.setItem('darkMode', 'false');
+          props.setDarkMode && props.setDarkMode(false);
+        }}
+      />
       <Icon icon="DarkMode" />
     </div>
   )
