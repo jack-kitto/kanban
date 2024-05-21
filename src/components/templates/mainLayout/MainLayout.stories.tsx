5697a76fbed00f1701b420b862b9040175b813af
jack-kitto1 <jack.kitto@thenod.app>
2024-05-21 21:06:35 +1000

    fix: add border lines to main layout

--- src/components/templates/mainLayout/MainLayout.stories.tsx
+++ src/components/templates/mainLayout/MainLayout.stories.tsx
@@ -10,10 +10,25 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const Light: Story = {
   args: {
     sidebar: <div className='w-full flex items-center justify-center h-full bg-lime-300'>Sidebar</div>,
     navbar: <div className='w-full flex items-center justify-center h-full bg-purple-300'>Navbar</div>,
     children: <div className='w-full flex items-center justify-center h-full bg-teal-400'>Children</div>
+  }
+};
+
+export const Dark: Story = {
+  args: {
+    sidebar: <div className='w-full flex items-center justify-center h-full bg-lime-300'>Sidebar</div>,
+    navbar: <div className='w-full flex items-center justify-center h-full bg-purple-300'>Navbar</div>,
+    children: <div className='w-full flex items-center justify-center h-full bg-teal-400'>Children</div>
+  },
+  render: (args) => {
+    return (
+      <div className='dark'>
+        <MainLayout {...args} />
+      </div>
+    )
   }
 };
--- src/components/templates/mainLayout/MainLayout.tsx
+++ src/components/templates/mainLayout/MainLayout.tsx
@@ -10,10 +10,10 @@ export default function MainLayout(props: MainLayoutProps): JSX.Element {
       <div className="max-sm:hidden sm:w-[261px] md:w-[300px] sm:visible sm:fixed sm:left-0 sm:top-0 h-full bottom-0 -z-50">
         {props.sidebar}
       </div>
-      <div className="fixed top-0 left-0 sm:left-[261px] md:left-[300px] h-[64px] md:h-[81px] right-0">
+      <div className="fixed top-0 left-0 sm:left-[261px] md:left-[300px] h-[64px] md:h-[81px] right-0  border-l-[1px] border-linesLight dark:border-linesDark">
         {props.navbar}
       </div>
-      <div className="fixed top-[64px] md:top-[81px] left-0 sm:left-[261px] md:left-[300px] right-0 bottom-0 -z-50">
+      <div className="fixed top-[64px] md:top-[81px] left-0 sm:left-[261px] md:left-[300px] right-0 bottom-0 -z-50 border-[1px] border-linesLight dark:border-linesDark">
         {props.children}
       </div>
     </div>
