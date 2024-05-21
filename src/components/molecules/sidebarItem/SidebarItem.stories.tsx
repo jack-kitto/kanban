fe10786a7fbf1fa2a9eddbb1892cf4e696747a7a
jack-kitto1 <jack.kitto@thenod.app>
2024-05-22 07:53:14 +1000

    fix: fix build warning

--- src/components/molecules/sidebarItem/SidebarItem.stories.tsx
+++ src/components/molecules/sidebarItem/SidebarItem.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import SidebarItem, { type SidebarItemProps } from './SidebarItem';
+import SidebarItem from './SidebarItem';
 
 const meta = {
   component: SidebarItem,
