5cc945b801d243cb04a55cbc6b89c80cb915d1a3
jack-kitto1 <jack.kitto@thenod.app>
2024-05-24 08:00:27 +1000

    fix: fix seed data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,28 +5,29 @@ const prisma = new PrismaClient()
 async function main() {
   await prisma.project.deleteMany()
   const fakeProjects = Array.from({ length: 2 }, fakeProject)
-  const fakeTasks = Array.from({ length: 10 }, fakeTask)
-  const fakeColumns = Array.from({ length: 3 }, fakeColumn)
-  const fakeSubtasks = Array.from({ length: 3 }, fakeSubtask)
-  const creatProjectInputs = fakeProjects.map((project: { title: string, description: string }, index: number): Prisma.ProjectCreateInput => {
+  const creatProjectInputs = fakeProjects.map((project: { title: string, description: string }, projectIndex: number): Prisma.ProjectCreateInput => {
     const columnPositions = generateNKeysBetween(null, null, 3)
+    const fakeColumns = Array.from({ length: 3 }, fakeColumn)
     return {
       title: project.title,
       description: project.description,
       columns: {
-        create: fakeColumns.map((column: { title: string, colour: string, position: string }, index: number) => {
+        create: fakeColumns.map((column: { title: string, colour: string, position: string }, colIndex: number) => {
           const taskPositions = generateNKeysBetween(null, null, 10)
+          console.log(taskPositions)
+          const fakeTasks = Array.from({ length: 10 }, fakeTask)
+          const fakeSubtasks = Array.from({ length: 3 }, fakeSubtask)
           return {
             title: column.title,
             colour: column.colour,
-            position: `${columnPositions[index]}`,
+            position: `${columnPositions[colIndex]}`,
             tasks: {
-              create: fakeTasks.map((task: { title: string, description: string, position: string }, index: number) => {
+              create: fakeTasks.map((task: { title: string, description: string, position: string }, taskIndex: number) => {
                 return {
                   title: task.title,
                   columnTitle: column.title,
                   description: task.description,
-                  position: `${taskPositions[index]}`,
+                  position: `${taskPositions[taskIndex]}`,
                   subtasks: {
                     create: fakeSubtasks.map((subtask: { title: string, completed: boolean }) => {
                       return {
