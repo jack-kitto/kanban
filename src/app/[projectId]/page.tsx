8509935c6d772a95fc4bf03a5972a7f0638374ff
jack-kitto1 <jack.kitto@thenod.app>
2024-05-24 08:05:02 +1000

    wip

--- src/app/[projectId]/page.tsx
+++ src/app/[projectId]/page.tsx
@@ -13,9 +13,15 @@ export default async function Page({ params }: { params: { projectId: string } }
   if (!params.projectId) {
     redirect('/')
   }
-  const project = await db.project.findFirst({
+
+  const projectsData = await db.project.findMany({
     where: {
-      id: `${params.projectId}`,
+      // TODO: Enable this when we can create projects or see projects that we are attached to users
+      // users: {
+      //   some: {
+      //     id: session.user.id
+      //   }
+      // }
     },
     include: {
       columns: {
@@ -29,46 +35,52 @@ export default async function Page({ params }: { params: { projectId: string } }
       }
     }
   })
+
+  const projects: Project[] = projectsData.map((project) => {
+    return {
+      id: project.id,
+      title: project.title,
+      description: project.description,
+      columns: project.columns.map((column) => {
+        return {
+          id: column.id,
+          title: column.title,
+          colour: column.colour as ColourName,
+          position: column.position,
+          tasks: column.tasks.map((task) => {
+            return {
+              id: task.id,
+              title: task.title,
+              description: task.description,
+              position: task.position,
+              columnId: task.columnId,
+              columnTitle: task.columnTitle,
+              subtasks: task.subtasks.map((subtask) => {
+                return {
+                  id: subtask.id,
+                  title: subtask.title,
+                  taskId: subtask.taskId,
+                  completed: subtask.completed
+                }
+              })
+            }
+          })
+        }
+      })
+    }
+  });
+
+  const project = projects.find((project: Project): boolean => project.id === params.projectId)
+
   if (!project) {
     redirect('/')
-  }
-  const project_: Project = {
-    id: project.id,
-    title: project.title,
-    description: project.description,
-    columns: project.columns.map((column) => {
-      return {
-        id: column.id,
-        title: column.title,
-        colour: column.colour as ColourName,
-        position: column.position,
-        tasks: column.tasks.map((task) => {
-          return {
-            id: task.id,
-            title: task.title,
-            description: task.description,
-            position: column.position,
-            columnId: task.columnId,
-            columnTitle: column.title,
-            subtasks: task.subtasks.map((subtask) => {
-              return {
-                id: subtask.id,
-                title: subtask.title,
-                taskId: subtask.taskId,
-                completed: subtask.completed
-              }
-            })
-          }
-        })
-      }
-    })
   }
 
   return (
     <div>
       <ProjectPage
-        project={project_}
-        projects={[]}
+        project={project}
+        projects={projects}
       />
     </div>
   );
--- src/components/pages/project/Project.tsx
+++ src/components/pages/project/Project.tsx
@@ -1,5 +1,6 @@
 "use client"
-import { useEffect, useMemo, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import { ColourName } from "~/components/atoms/bubble/Bubble";
 import { updateTaskInListOfColumns } from "~/components/helpers";
 import { Board, Navbar } from "~/components/organisms";
@@ -16,11 +17,13 @@ export default function ProjectPage(props: ProjectPageProps): JSX.Element {
   const [currentProject, setCurrentProject] = useState<Project | null>(props.project)
   const [isClient, setIsClient] = useState(false)
   const [columns, setColumns] = useState<ColumnType[]>([])
+  const [sidebarHidden, setSidebarHidden] = useState<boolean>(false)
+  const router = useRouter()
 
   useEffect(() => {
     const data = currentProject?.columns.map((column) => ({
       // trim to  10 characters
-      title: column.title.length > 10 ? column.title.slice(0, 10) + "..." : column.title,
+      title: column.title.length > 20 ? column.title.slice(0, 20) + "..." : column.title,
       id: column.id,
       position: column.position,
       colour: column.colour as ColourName,
@@ -49,26 +52,34 @@ export default function ProjectPage(props: ProjectPageProps): JSX.Element {
   if (!isClient) {
     return (<></>)
   }
+
+  if (!currentProject) {
+    return (<></>)
+  }
   return (
     <MainLayout
-      sidebarHidden={false}
+      sidebarHidden={sidebarHidden}
       sidebar={
         <Sidebar
+          currentProject={currentProject}
           projects={props.projects}
-          onClickProject={() => { }}
+          onClickProject={(project: Project): void => setCurrentProject(project)}
           setCreateProjectOpen={() => { }}
-          setSidebarHidden={() => { }}
-          sidebarHidden={false}
+          setSidebarHidden={setSidebarHidden}
+          sidebarHidden={sidebarHidden}
+          onLogoClick={() => router.push('/')}
         />
       }
       navbar={
         <Navbar
-          updateTask={() => { }}
-          onDeleteTask={() => { }}
+          sidebarOpen={!sidebarHidden}
+          project={currentProject}
+          updateTask={(task: TaskType): void => updateTaskInListOfColumns(task, columns, (newColumns: ColumnType[]): void => setColumns(newColumns))}
+          onDeleteTask={(task: TaskType): void => setColumns(columns.map((c: ColumnType): ColumnType => c.id === task.columnId ? { ...c, tasks: c.tasks.filter((t: TaskType): boolean => t.id !== task.id) } : c))}
         />
       }
     >
-      <div className="w-full h-full p-6 bg-linesLight">
+      <div className="w-full h-full p-6 bg-lightGray dark:bg-veryDarkGray">
         <Board
           updateTask={(task: TaskType): void => updateTaskInListOfColumns(task, columns, (newColumns: ColumnType[]): void => setColumns(newColumns))}
           onDeleteTask={(task: TaskType): void => setColumns(columns.map((c: ColumnType): ColumnType => c.id === task.columnId ? { ...c, tasks: c.tasks.filter((t: TaskType): boolean => t.id !== task.id) } : c))}
