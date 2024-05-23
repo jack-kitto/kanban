fd97f6d701843bf9b7d5f9177b784b208f35136a
jack-kitto1 <jack.kitto@thenod.app>
2024-05-24 06:53:35 +1000

    fix: Fix positioning of layouts so they appear behind popups

--- src/components/templates/homeLayout/HomeLayout.tsx
+++ src/components/templates/homeLayout/HomeLayout.tsx
@@ -9,7 +9,7 @@ export default function HomeLayout(props: HomeLayoutProps): JSX.Element {
       <div className="fixed top-0 left-0 h-[64px] md:h-[81px] right-0">
         {props.navbar}
       </div>
-      <div className="fixed top-[64px] md:top-[81px] left-0 right-0 bottom-0">
+      <div className="fixed -z-20 top-[64px] md:top-[81px] left-0 right-0 bottom-0 bg-linesLight dark:bg-darkGray">
         {props.children}
       </div>
     </div>
--- src/components/templates/mainLayout/MainLayout.tsx
+++ src/components/templates/mainLayout/MainLayout.tsx
@@ -8,13 +8,13 @@ export interface MainLayoutProps {
 export default function MainLayout(props: MainLayoutProps): JSX.Element {
   return (
     <div className="">
-      <div className={`max-sm:hidden z-20 sm:w-[261px] md:w-[300px] sm:visible sm:fixed sm:left-0 sm:top-0 h-full bottom-0 ${!props.sidebarHidden && '-z-50'}`}>
+      <div className={`max-sm:hidden -z-20 sm:w-[261px] md:w-[300px] sm:visible sm:fixed sm:left-0 sm:top-0 h-full bottom-0 ${!props.sidebarHidden && '-z-50'}`}>
         {props.sidebar}
       </div>
       <div className={`fixed top-0 left-0 ${!props.sidebarHidden && 'sm:left-[261px] md:left-[300px]'} h-[64px] md:h-[81px] right-0  border-l-[1px] border-r-linesLight border-t-border-r-linesLight dark:border-linesDark`}>
         {props.navbar}
       </div>
-      <div className={`fixed z-10 top-[64px] md:top-[81px] left-0 ${!props.sidebarHidden && 'sm:left-[261px] md:left-[300px]'} right-0 bottom-0 border-[1px] border-linesLight dark:border-linesDark`}>
+      <div className={`fixed -z-10 top-[64px] md:top-[81px] left-0 ${!props.sidebarHidden && 'sm:left-[261px] md:left-[300px]'} right-0 bottom-0 border-[1px] border-linesLight dark:border-linesDark`}>
         {props.children}
       </div>
     </div>
