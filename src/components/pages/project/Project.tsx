d1b68fa4afd59bb15aa94b7288567c84579da875
jack-kitto1 <jack.kitto@thenod.app>
2024-05-25 07:43:07 +1000

    fix: Move navbar add task modal into project component so it appears over the project board

--- src/components/pages/project/Project.tsx
+++ src/components/pages/project/Project.tsx
@@ -1,8 +1,9 @@
 "use client"
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { Modal } from "~/components/atoms";
 import { updateTaskInListOfColumns } from "~/components/helpers";
-import { Board, Navbar } from "~/components/organisms";
+import { Board, Navbar, TaskDetail } from "~/components/organisms";
 import Sidebar from "~/components/organisms/sidebar/Sidebar";
 import { MainLayout } from "~/components/templates";
 import type { ColumnType, Project, TaskType } from "~/components/types";
@@ -18,6 +19,9 @@ export default function ProjectPage(props: ProjectPageProps): JSX.Element {
   const [, setCreateProjectOpen] = useState<boolean>(false)
   const [columns, setColumns] = useState<ColumnType[]>([])
   const [sidebarHidden, setSidebarHidden] = useState<boolean>(false)
+  const [showTaskDetail, setShowTaskDetail] = useState<boolean>(false)
+  const [editing, setEditing] = useState<boolean>(true)
+  const [newTask, setNewTask] = useState<boolean>(true)
   const router = useRouter()
 
   useEffect(() => {
@@ -57,36 +61,65 @@ export default function ProjectPage(props: ProjectPageProps): JSX.Element {
     return (<></>)
   }
   return (
-    <MainLayout
-      sidebarHidden={sidebarHidden}
-      sidebar={
-        <Sidebar
-          currentProject={currentProject}
-          projects={props.projects}
-          onClickProject={(project: Project): void => setCurrentProject(project)}
-          setCreateProjectOpen={setCreateProjectOpen}
-          setSidebarHidden={setSidebarHidden}
-          sidebarHidden={sidebarHidden}
-          onLogoClick={() => router.push('/')}
-        />
-      }
-      navbar={
-        <Navbar
-          sidebarOpen={!sidebarHidden}
-          project={currentProject}
-          updateTask={(task: TaskType): void => updateTaskInListOfColumns(task, columns, (newColumns: ColumnType[]): void => setColumns(newColumns))}
-          onDeleteTask={(task: TaskType): void => setColumns(columns.map((c: ColumnType): ColumnType => c.id === task.columnId ? { ...c, tasks: c.tasks.filter((t: TaskType): boolean => t.id !== task.id) } : c))}
-        />
-      }
-    >
-      <div className="w-full h-full p-6 bg-lightGray dark:bg-veryDarkGray">
-        <Board
-          updateTask={(task: TaskType): void => updateTaskInListOfColumns(task, columns, (newColumns: ColumnType[]): void => setColumns(newColumns))}
-          onDeleteTask={(task: TaskType): void => setColumns(columns.map((c: ColumnType): ColumnType => c.id === task.columnId ? { ...c, tasks: c.tasks.filter((t: TaskType): boolean => t.id !== task.id) } : c))}
-          updateColumns={(columns: ColumnType[]): void => setColumns(columns)}
+    <>
+      <Modal
+        open={showTaskDetail}
+        close={(): void => {
+          setShowTaskDetail(false)
+          setEditing(true)
+          setNewTask(true)
+        }}
+      >
+        <TaskDetail
           columns={columns}
+          newTask={newTask}
+          editing={editing}
+          setEditing={setEditing}
+          menuOptions={[]}
+          saveChanges={(task: TaskType): void => {
+            updateTaskInListOfColumns(task, columns, (newColumns: ColumnType[]): void => setColumns(newColumns))
+            setShowTaskDetail(false)
+            setNewTask(false)
+
+            //
+            // TODO: make sure to caculate the new position based on the column chosen and the last task in that column
+            //
+
+          }}
         />
-      </div>
-    </MainLayout>
+      </Modal>
+      <MainLayout
+        sidebarHidden={sidebarHidden}
+        sidebar={
+          <Sidebar
+            currentProject={currentProject}
+            projects={props.projects}
+            onClickProject={(project: Project): void => setCurrentProject(project)}
+            setCreateProjectOpen={setCreateProjectOpen}
+            setSidebarHidden={setSidebarHidden}
+            sidebarHidden={sidebarHidden}
+            onLogoClick={() => router.push('/')}
+          />
+        }
+        navbar={
+          <Navbar
+            onAddTask={(): void => setShowTaskDetail(true)}
+            sidebarOpen={!sidebarHidden}
+            project={currentProject}
+            updateTask={(task: TaskType): void => updateTaskInListOfColumns(task, columns, (newColumns: ColumnType[]): void => setColumns(newColumns))}
+            onDeleteTask={(task: TaskType): void => setColumns(columns.map((c: ColumnType): ColumnType => c.id === task.columnId ? { ...c, tasks: c.tasks.filter((t: TaskType): boolean => t.id !== task.id) } : c))}
+          />
+        }
+      >
+        <div className="w-full h-full p-6 bg-lightGray dark:bg-veryDarkGray">
+          <Board
+            updateTask={(task: TaskType): void => updateTaskInListOfColumns(task, columns, (newColumns: ColumnType[]): void => setColumns(newColumns))}
+            onDeleteTask={(task: TaskType): void => setColumns(columns.map((c: ColumnType): ColumnType => c.id === task.columnId ? { ...c, tasks: c.tasks.filter((t: TaskType): boolean => t.id !== task.id) } : c))}
+            updateColumns={(columns: ColumnType[]): void => setColumns(columns)}
+            columns={columns}
+          />
+        </div>
+      </MainLayout>
+    </>
   )
 }
