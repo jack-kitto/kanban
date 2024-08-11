f4bc154bc1d43633c1d8e3ff409a23efe9172c26
Jack Kitto <Jack Kitto>
2024-08-11 21:09:26 +1000

    make popover menu close after clicking an option

--- src/components/molecules/popoverMenu/PopoverMenu.tsx
+++ src/components/molecules/popoverMenu/PopoverMenu.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from "react";
 import { Popover, type PopoverProps, PopoverTrigger, PopoverContent, Button } from "@nextui-org/react";
 import { MenuButton } from "~/components/atoms";
@@ -13,9 +14,11 @@ export interface PopoverMenuProps {
 }
 
 export default function PopoverMenu(props: PopoverMenuProps): JSX.Element {
+  const [isOpen, setIsOpen] = React.useState(false);
+
   return (
     <div className="flex justify-end items-end">
-      <Popover placement="left-start" showArrow={true}>
+      <Popover isOpen={isOpen} onOpenChange={(open) => setIsOpen(open)} placement="left-start" showArrow={true}>
         <PopoverTrigger>
           <Button className="bg-transparent">
             <MenuButton type="click" />
@@ -24,7 +27,14 @@ export default function PopoverMenu(props: PopoverMenuProps): JSX.Element {
         <PopoverContent>
           <div className={`flex flex-col px-[17px] py-5 min-w-[192px] items-start`}>
             {props.options.map((option: PopoverMenuOption, index: number): JSX.Element => (
-              <Button key={index} onMouseDown={option.onClick} className={`bg-white w-full justify-start dark:bg-veryDarkGray p-2 select-none prose-bl ${option.destructive ? 'text-red' : 'text-mediumGray dark:text-white'}`}>
+              <Button
+                key={index}
+                onClick={() => {
+                  setIsOpen(false)
+                  option.onClick()
+                }}
+                className={`bg-white w-full justify-start dark:bg-veryDarkGray p-2 select-none prose-bl ${option.destructive ? 'text-red' : 'text-mediumGray dark:text-white'}`}
+              >
                 {option.text}
               </Button>
             ))}
