fd62245fdc2ddba6ad073b67c160d734e126d6de
Jack Kitto <Jack Kitto>
2024-08-11 20:04:03 +1000

    create task router

---
+++ src/server/api/routers/task.ts
@@ -0,0 +1,39 @@
+import { z } from "zod";
+import { Subtask, taskTypeSchema } from "~/components/types";
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+
+export const taskRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+  create: protectedProcedure
+    .input(taskTypeSchema)
+    .mutation(async ({ ctx, input }) => {
+      ctx.session.user.id
+      return ctx.db.task.create({
+        data: {
+          title: input.title,
+          description: input.description,
+          id: input.id,
+          columnTitle: input.columnTitle,
+          columnId: input.columnId,
+          position: input.position,
+          subtasks: {
+            create: input.subtasks.map((s: Subtask) => ({
+              title: s.title,
+              id: s.id,
+              completed: s.completed
+            }))
+          }
+        },
+      });
+    }),
+});
